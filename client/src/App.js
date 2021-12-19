35f4cd8193a605ad8fed75b922f0b573e0ed9fd7
Jake Sweeney <jakemsweeney12@gmail.com>
2021-12-19 00:01:54 -0500

    Hello message animation implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, {useState, useEffect} from 'react'
-import './App.css';
+import './styles/body.css';
+import './styles/menu.css';
+import bubble from './assets/white-bubble.png';
 
 function App() {
-    const [data, setData] = useState([{}]);
+    const [data, setData] = useState(0);
 
     useEffect(() => {
-        fetch("/jakesweeney/home").then(
-            res => res.json()
-        ).then(
+        fetch("/jakesweeney/home", {
+            headers : {
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+            }
+        })
+        .then(
+            res => res.status
+        )
+        .then(
             data => {
                 setData(data)
                 console.log(data)
@@ -15,15 +24,18 @@ function App() {
         )
     }, [])
 
+    //console.log(data);
     return (
         <div>
-            {(typeof data.members === 'undefined') ? (
-                <p>Loading...</p>
+            {(data !== 201) ? (
+                <p>ERROR: {data}</p>
             ) : (
-                <div className="hello-message">Hi, my name is Jake</div>
+                <div>
+                    <div className="hello-message">Hi, my name is Jake</div>
+                </div>
             )}
         </div>
     );
 }
-
+//<img src={bubble} alt="bubble" />
 export default App;
